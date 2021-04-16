09f5f667bd326b87a28ffde40ea384f01239121d
16t1021064 <16t1021064@husc.edu.vn>
2021-04-16 15:40:16 +0700

    CityDAL

--- LiteCommerce.Admin/Controllers/TestController.cs
+++ LiteCommerce.Admin/Controllers/TestController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using LiteCommerce.DataLayers;
 using LiteCommerce.DomainModels;
+using System.Configuration;
+using LiteCommerce.DataLayers.SQLServer;
 
 namespace LiteCommerce.Admin.Controllers
 {
@@ -13,7 +15,10 @@ namespace LiteCommerce.Admin.Controllers
         // GET: Test
         public ActionResult Index()
         {
-            return View();
+            string connectionString = ConfigurationManager.ConnectionStrings["LiteCommerceDB"].ConnectionString;
+            ICountryDAL dal = new CountryDAL(connectionString);
+            var data = dal.List();
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ LiteCommerce.DataLayers/SQLServer/CityDAL.cs
@@ -0,0 +1,53 @@
+﻿using LiteCommerce.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCommerce.DataLayers.SQLServer
+{
+    public class CityDAL : _BaseDAL, ICityDAL
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public CityDAL(string connectionString) : base(connectionString)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="countryName"></param>
+        /// <returns></returns>
+        public List<City> List(string countryName)
+        {
+            List<City> data = new List<City>();
+            using (SqlConnection cn = GetConnection())
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "select * from Cities";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Connection = cn;
+                using (SqlDataReader dbReader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection))
+                {
+                    while (dbReader.Read())
+                    {
+                        data.Add(new City()
+                        {
+                            CityName = Convert.ToString(dbReader["CityName"]),
+                            CountryName = Convert.ToString(dbReader["CountryName"])
+                        });
+                    }
+                }
+
+                cn.Close();
+            }
+
+            return data;
+        }
+    }
+}
