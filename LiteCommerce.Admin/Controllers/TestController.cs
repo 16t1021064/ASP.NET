ae8e7b94550f6c3bc019114906e8841bcf67f75e
16t1021064 <16t1021064@husc.edu.vn>
2021-04-16 15:20:53 +0700

    test controller

---
+++ LiteCommerce.Admin/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LiteCommerce.DataLayers;
+using LiteCommerce.DomainModels;
+
+namespace LiteCommerce.Admin.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
